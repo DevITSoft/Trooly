594bd0c83a538cd9bc0e6b6d5a83e6cb9360f039
DevITSoft <devitsoftwares@gmail.com>
2019-09-24 22:46:32 -0300

    Home

--- app/src/main/java/com/devit/mobile/trooly/MainActivity.java
+++ app/src/main/java/com/devit/mobile/trooly/MainActivity.java
@@ -1,6 +1,9 @@
 package com.devit.mobile.trooly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), Profile.class));
+                finish();
+            }
+        }, 3000);
     }
 }
